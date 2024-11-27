fd0528e8b89e101532c25a68eea62a4def1cda11
ghosart <ghosart.office@gmail.com>
2024-11-28 02:52:51 +0900

    update/home_screenの設置

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/screens/home_screen.dart'; // HomeScreenのインポート
-import '../models/custom_icon.dart'; // CustomIcons のインポート
+import 'package:flutter_app/screens/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -14,16 +13,16 @@ class SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    // 初期化処理を非同期で実行
-    _initializeApp();
+    // ビルド完了後に初期化処理を呼び出す
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _initializeApp();
+    });
   }
 
   // 初期化処理を行い、その後画面遷移する非同期メソッド
   Future<void> _initializeApp() async {
-    // まずプリロード処理
-    await CustomIcons.preloadIcons();
-
-    // その後、他の初期化処理があればここに追加
+    // 必要な非同期処理を実行
+    await Future.delayed(const Duration(seconds: 2)); // ダミーの遅延処理
 
     // 初期化処理が終わったら、HomeScreenに遷移
     if (mounted) {
--- lib/theme/theme_data.dart
+++ lib/theme/theme_data.dart
@@ -4,17 +4,27 @@ ThemeData buildThemeData() {
   return ThemeData(
     brightness: Brightness.dark,
     primaryColor: const Color(0xFF0D0D0D), // 宇宙の暗い背景
-    scaffoldBackgroundColor: const Color(0xFF000814), // 宇宙の深い背景
+    scaffoldBackgroundColor: const Color(0xFF0D0D0D), // 宇宙の深い背景
     colorScheme: const ColorScheme(
       brightness: Brightness.dark,
       primary: Colors.cyan,
-      secondary: Color(0xFF001D3D),
-      surface: Color(0xFF3C434B),
-      onSecondary: Colors.white,
-      onSurface: Colors.white70,
+      secondary: Color.fromARGB(100, 0, 0, 0),
+      onSecondary: Color.fromARGB(255, 0, 0, 0),
+      surface: Color.fromARGB(30, 255, 255, 255),
+      onSurface: Color.fromARGB(100, 255, 255, 255),
       error: Color(0xFFEE4266),
       onError: Colors.white,
       onPrimary: Colors.white,
+    ),
+    iconTheme: IconThemeData(color: Colors.cyan[900]),
+    primaryIconTheme: const IconThemeData(color: Colors.cyan),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color.fromARGB(100, 0, 0, 0),
+      titleTextStyle: TextStyle(
+        color: Color.fromARGB(200, 255, 255, 255),
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),
     ),
     textTheme: const TextTheme(
       displayLarge: TextStyle(
@@ -33,8 +43,20 @@ ThemeData buildThemeData() {
       ),
     ),
     buttonTheme: const ButtonThemeData(
-      buttonColor: Color(0xFF61C0BF), // 優しい青緑
+      buttonColor: Colors.cyan,
       textTheme: ButtonTextTheme.primary,
+    ),
+    dialogTheme: const DialogTheme(
+      backgroundColor: Color.fromARGB(255, 30, 30, 30),
+      titleTextStyle: TextStyle(
+        color: Color.fromARGB(200, 255, 255, 255),
+        fontSize: 20, // フォントサイズを変更
+        fontWeight: FontWeight.bold, // 太字に設定
+      ),
+      contentTextStyle: TextStyle(
+        color: Color.fromARGB(100, 255, 255, 255),
+        fontSize: 16, // フォントサイズを変更
+      ),
     ),
   );
 }
